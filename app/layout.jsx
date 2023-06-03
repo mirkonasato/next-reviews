e62330357f598765748e7f5294fb3196d540faae
Mirko Nasato <mirko@nasato.dev>
2023-06-03 11:00:00 +0100

    Link Component

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,23 @@
+import Link from 'next/link';
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
         <header>
-          [header]
+          <nav>
+            <ul>
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="/reviews">Reviews</Link>
+              </li>
+              <li>
+                <Link href="/about">About</Link>
+              </li>
+            </ul>
+          </nav>
         </header>
         <main>
           {children}
