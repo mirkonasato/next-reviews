14358393f8a690b270597bb9ad037764b627eacb
Mirko Nasato <mirko@nasato.dev>
2023-06-03 11:00:00 +0100

    Prefetching

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
                 <Link href="/reviews">Reviews</Link>
               </li>
               <li>
-                <Link href="/about">About</Link>
+                <Link href="/about" prefetch={false}>About</Link>
               </li>
             </ul>
           </nav>
