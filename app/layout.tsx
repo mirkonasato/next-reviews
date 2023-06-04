d96fa73e804a62afb2d66223e9dec20596b4ee1f
Mirko Nasato <mirko@nasato.dev>
2023-06-04 11:00:00 +0100

    Color Palette

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { ReactNode } from 'react';
-import Link from 'next/link';
+import NavBar from '../components/NavBar';
 import './globals.css';
 
 interface LayoutProps {
@@ -9,28 +9,19 @@ interface LayoutProps {
 export default function RootLayout({ children }: LayoutProps) {
   return (
     <html lang="en">
-      <body className="flex flex-col px-4 py-2 min-h-screen">
+      <body className="bg-orange-50 flex flex-col px-4 py-2 min-h-screen">
         <header>
-          <nav>
-            <ul className="flex gap-2">
-              <li>
-                <Link href="/">Home</Link>
-              </li>
-              <li>
-                <Link href="/reviews">Reviews</Link>
-              </li>
-              <li>
-                <Link href="/about" prefetch={false}>About</Link>
-              </li>
-            </ul>
-          </nav>
+          <NavBar />
         </header>
         <main className="grow py-3">
           {children}
         </main>
         <footer className="border-t py-3 text-center text-xs">
-          Game data and images courtesy
-          of <a href="https://rawg.io/" target="_blank">RAWG</a>
+          Game data and images courtesy of{' '}
+          <a href="https://rawg.io/" target="_blank"
+            className="text-orange-800 hover:underline">
+            RAWG
+          </a>
         </footer>
       </body>
     </html>
