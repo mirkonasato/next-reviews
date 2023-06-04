3f7855e691555198137bd4ad7f5825be57c67103
Mirko Nasato <mirko@nasato.dev>
2023-06-04 11:00:00 +0100

    Installing Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+	/** @type {import('tailwindcss').Config} */
+  module.exports = {
+    content: [
+      './app/**/*.{jsx,tsx}',
+      './components/**/*.{jsx,tsx}',
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
