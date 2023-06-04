147e6b5b6961d80e4a16a864d828f0a7a783c4c2
Mirko Nasato <mirko@nasato.dev>
2023-06-04 11:00:00 +0100

    Layout Styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,10 +9,10 @@ interface LayoutProps {
 export default function RootLayout({ children }: LayoutProps) {
   return (
     <html lang="en">
-      <body>
+      <body className="flex flex-col px-4 py-2 min-h-screen">
         <header>
           <nav>
-            <ul>
+            <ul className="flex gap-2">
               <li>
                 <Link href="/">Home</Link>
               </li>
@@ -25,10 +25,10 @@ export default function RootLayout({ children }: LayoutProps) {
             </ul>
           </nav>
         </header>
-        <main>
+        <main className="grow py-3">
           {children}
         </main>
-        <footer>
+        <footer className="border-t py-3 text-center text-xs">
           Game data and images courtesy
           of <a href="https://rawg.io/" target="_blank">RAWG</a>
         </footer>
