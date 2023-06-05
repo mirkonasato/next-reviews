bb2624e1367817f8475965f24c76921df17164a2
Mirko Nasato <mirko@nasato.dev>
2023-06-05 11:00:00 +0100

    Responsive Design

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import Heading from '@/components/Heading';
 
 export default function HomePage() {
@@ -5,9 +6,22 @@ export default function HomePage() {
   return (
     <>
       <Heading>Indie Gamer</Heading>
-      <p>
+      <p className="pb-3">
         Only the best indie games, reviewed for you.
       </p>
+      <div className="bg-white border rounded shadow w-80
+                      hover:shadow-xl sm:w-full">
+        <Link href="/reviews/stardew-valley"
+          className="flex flex-col sm:flex-row">
+          <img src="/images/stardew-valley.jpg" alt=""
+            width="320" height="180"
+            className="rounded-t sm:rounded-l sm:rounded-r-none"
+          />
+          <h2 className="font-orbitron font-semibold py-1 text-center sm:px-2">
+            Stardew Valley
+          </h2>
+        </Link>
+      </div>
     </>
   );
 }
