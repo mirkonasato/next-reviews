cbb4db65c0fb3a6b0826dc0d90410ced7f1c7841
Mirko Nasato <mirko@nasato.dev>
2023-06-06 11:00:00 +0100

    Data Layer Separation

--- app/reviews/stardew-valley/page.tsx
+++ app/reviews/stardew-valley/page.tsx
@@ -1,21 +1,16 @@
-import { readFile } from 'node:fs/promises';
-import matter from 'gray-matter';
-import { marked } from 'marked';
 import Heading from '@/components/Heading';
+import { getReview } from '@/lib/reviews';
 
 export default async function StardewValleyPage() {
-  const text = await readFile('./content/reviews/stardew-valley.md', 'utf8');
-  const { content, data: { title, date, image } } = matter(text);
-  const html = marked(content, { headerIds: false, mangle: false });
-
+  const review = await getReview('stardew-valley');
   return (
     <>
-      <Heading>{title}</Heading>
-      <p className="italic pb-2">{date}</p>
-      <img src={image} alt=""
+      <Heading>{review.title}</Heading>
+      <p className="italic pb-2">{review.date}</p>
+      <img src={review.image} alt=""
         width="640" height="360" className="mb-2 rounded"
       />
-      <article dangerouslySetInnerHTML={{ __html: html }}
+      <article dangerouslySetInnerHTML={{ __html: review.body }}
         className="max-w-screen-sm prose prose-slate"
       />
     </>
