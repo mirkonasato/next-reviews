0a85bf739851a405b350a36b74abcba1d2df43c9
Mirko Nasato <mirko@nasato.dev>
2023-06-06 11:00:00 +0100

    Front Matter

--- app/reviews/stardew-valley/page.jsx
+++ app/reviews/stardew-valley/page.jsx
@@ -1,15 +1,18 @@
 import { readFile } from 'node:fs/promises';
+import matter from 'gray-matter';
 import { marked } from 'marked';
 import Heading from '@/components/Heading';
 
 export default async function StardewValleyPage() {
   const text = await readFile('./content/reviews/stardew-valley.md', 'utf8');
-  const html = marked(text, { headerIds: false, mangle: false });
+  const { content, data: { title, date, image } } = matter(text);
+  const html = marked(content, { headerIds: false, mangle: false });
 
   return (
     <>
-      <Heading>Stardew Valley</Heading>
-      <img src="/images/stardew-valley.jpg" alt=""
+      <Heading>{title}</Heading>
+      <p className="italic pb-2">{date}</p>
+      <img src={image} alt=""
         width="640" height="360" className="mb-2 rounded"
       />
       <article dangerouslySetInnerHTML={{ __html: html }}
