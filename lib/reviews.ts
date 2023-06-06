8b6d609a0b79fee357f88f38c7ce6d82523f2307
Mirko Nasato <mirko@nasato.dev>
2023-06-06 11:00:00 +0100

    Listing Files

--- lib/reviews.ts
+++ lib/reviews.ts
@@ -1,8 +1,9 @@
-import { readFile } from 'node:fs/promises';
+import { readdir, readFile } from 'node:fs/promises';
 import matter from 'gray-matter';
 import { marked } from 'marked';
 
 export interface Review {
+  slug: string;
   title: string;
   date: string;
   image: string;
@@ -13,5 +14,17 @@ export async function getReview(slug: string): Promise<Review> {
   const text = await readFile(`./content/reviews/${slug}.md`, 'utf8');
   const { content, data: { title, date, image } } = matter(text);
   const body = marked(content, { headerIds: false, mangle: false });
-  return { title, date, image, body };
+  return { slug, title, date, image, body };
+}
+
+export async function getReviews(): Promise<Review[]> {
+  const files = await readdir('./content/reviews');
+  const slugs = files.filter((file) => file.endsWith('.md'))
+    .map((file) => file.slice(0, -'.md'.length));
+  const reviews: Review[] = [];
+  for (const slug of slugs) {
+    const review = await getReview(slug);
+    reviews.push(review);
+  }
+  return reviews;
 }
