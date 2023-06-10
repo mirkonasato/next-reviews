082574e917a346e01bb1c8be831da629db65349c
Mirko Nasato <mirko@nasato.dev>
2023-06-10 11:00:00 +0100

    Get Review From CMS - Step 1

--- scripts/strapi-request.mjs
+++ scripts/strapi-request.mjs
@@ -3,10 +3,10 @@ import qs from 'qs';
 
 const url = 'http://localhost:1337/api/reviews'
   + '?' + qs.stringify({
-    fields: ['slug', 'title', 'subtitle', 'publishedAt'],
+    filters: { slug: { $eq: 'hades-2018' } },
+    fields: ['slug', 'title', 'subtitle', 'publishedAt', 'body'],
     populate: { image: { fields: ['url'] } },
-    sort: ['publishedAt:desc'],
-    pagination: { pageSize: 6 },
+    pagination: { pageSize: 1, withCount: false },
   }, { encodeValuesOnly: true });
 console.log('url:', url);
 const response = await fetch(url);
