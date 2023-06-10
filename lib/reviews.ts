f31183b7649078e17f6d0c5abb88df5511b8d113
Mirko Nasato <mirko@nasato.dev>
2023-06-10 11:00:00 +0100

    Get Review From CMS - Step 2

--- lib/reviews.ts
+++ lib/reviews.ts
@@ -1,5 +1,4 @@
-import { readdir, readFile } from 'node:fs/promises';
-import matter from 'gray-matter';
+import { readdir } from 'node:fs/promises';
 import { marked } from 'marked';
 import qs from 'qs';
 
@@ -22,10 +21,24 @@ export async function getFeaturedReview(): Promise<Review> {
 }
 
 export async function getReview(slug: string): Promise<FullReview> {
-  const text = await readFile(`./content/reviews/${slug}.md`, 'utf8');
-  const { content, data: { title, date, image } } = matter(text);
-  const body = marked(content, { headerIds: false, mangle: false });
-  return { slug, title, date, image, body };
+  const url = `${CMS_URL}/api/reviews?`
+    + qs.stringify({
+      filters: { slug: { $eq: slug } },
+      fields: ['slug', 'title', 'subtitle', 'publishedAt', 'body'],
+      populate: { image: { fields: ['url'] } },
+      pagination: { pageSize: 1, withCount: false },
+    }, { encodeValuesOnly: true });
+  console.log('getReview:', url);
+  const response = await fetch(url);
+  const { data } = await response.json();
+  const { attributes } = data[0];
+  return {
+    slug: attributes.slug,
+    title: attributes.title,
+    date: attributes.publishedAt.slice(0, 'yyyy-mm-dd'.length),
+    image: CMS_URL + attributes.image.data.attributes.url,
+    body: marked(attributes.body, { headerIds: false, mangle: false }),
+  };
 }
 
 export async function getReviews(): Promise<Review[]> {
