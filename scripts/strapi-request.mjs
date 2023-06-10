f236fe573fa5900c9850afa1f7d9e8b4a90d8d56
Mirko Nasato <mirko@nasato.dev>
2023-06-10 11:00:00 +0100

    Strapi API Parameters

--- scripts/strapi-request.mjs
+++ scripts/strapi-request.mjs
@@ -1,7 +1,14 @@
 import { writeFileSync } from 'node:fs';
+import qs from 'qs';
 
 const url = 'http://localhost:1337/api/reviews'
-  + '?populate=*';
+  + '?' + qs.stringify({
+    fields: ['slug', 'title', 'subtitle', 'publishedAt'],
+    populate: { image: { fields: ['url'] } },
+    sort: ['publishedAt:desc'],
+    pagination: { pageSize: 6 },
+  }, { encodeValuesOnly: true });
+console.log('url:', url);
 const response = await fetch(url);
 const body = await response.json();
 const formatted = JSON.stringify(body, null, 2);
