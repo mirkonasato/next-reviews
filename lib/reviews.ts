6383ce660049ad122712fd62f943d6e902d5525d
Mirko Nasato <mirko@nasato.dev>
2023-06-11 11:00:00 +0100

    Exercise: Home Images

--- lib/reviews.ts
+++ lib/reviews.ts
@@ -19,11 +19,6 @@ export interface FullReview extends Review {
   body: string;
 }
 
-export async function getFeaturedReview(): Promise<Review> {
-  const reviews = await getReviews();
-  return reviews[0];
-}
-
 export async function getReview(slug: string): Promise<FullReview> {
   const { data } = await fetchReviews({
     filters: { slug: { $eq: slug } },
@@ -38,12 +33,12 @@ export async function getReview(slug: string): Promise<FullReview> {
   };
 }
 
-export async function getReviews(): Promise<Review[]> {
+export async function getReviews(pageSize: number): Promise<Review[]> {
   const { data } = await fetchReviews({
     fields: ['slug', 'title', 'subtitle', 'publishedAt'],
     populate: { image: { fields: ['url'] } },
     sort: ['publishedAt:desc'],
-    pagination: { pageSize: 6 },
+    pagination: { pageSize },
   });
   return data.map(toReview);
 }
