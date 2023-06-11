4262fce7ed1b5118509c5aba67e7c77a7d94b335
Mirko Nasato <mirko@nasato.dev>
2023-06-11 11:00:00 +0100

    Exercise: Home Images

--- app/reviews/page.jsx
+++ app/reviews/page.jsx
@@ -8,7 +8,7 @@ export const metadata = {
 };
 
 export default async function ReviewsPage() {
-  const reviews = await getReviews();
+  const reviews = await getReviews(6);
   // console.log('[ReviewsPage] reviews:', reviews);
   return (
     <>
