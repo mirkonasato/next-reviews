5d5b72975520341578feedfa944d2c6c86293b84
Mirko Nasato <mirko@nasato.dev>
2023-06-11 11:00:00 +0100

    Lazy Loading and Priority

--- app/reviews/[slug]/page.jsx
+++ app/reviews/[slug]/page.jsx
@@ -26,7 +26,7 @@ export default async function ReviewPage({ params: { slug } }) {
         <p className="italic pb-2">{review.date}</p>
         <ShareLinkButton />
       </div>
-      <Image src={review.image} alt=""
+      <Image src={review.image} alt="" priority
         width="640" height="360" className="mb-2 rounded"
       />
       <article dangerouslySetInnerHTML={{ __html: review.body }}
