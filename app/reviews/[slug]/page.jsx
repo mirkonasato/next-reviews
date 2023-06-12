7715490fd21036cfe340384a152d22dd49721e75
Mirko Nasato <mirko@nasato.dev>
2023-06-12 11:00:00 +0100

    Background Revalidation

--- app/reviews/[slug]/page.jsx
+++ app/reviews/[slug]/page.jsx
@@ -4,13 +4,11 @@ import Heading from '@/components/Heading';
 import ShareLinkButton from '@/components/ShareLinkButton';
 import { getReview, getSlugs } from '@/lib/reviews';
 
-export const dynamic = 'force-dynamic';
-
-// export async function generateStaticParams() {
-//   const slugs = await getSlugs();
-//   // console.log('[ReviewPage] generateStaticParams:', slugs);
-//   return slugs.map((slug) => ({ slug }));
-// }
+export async function generateStaticParams() {
+  const slugs = await getSlugs();
+  // console.log('[ReviewPage] generateStaticParams:', slugs);
+  return slugs.map((slug) => ({ slug }));
+}
 
 export async function generateMetadata({ params: { slug } }) {
   const review = await getReview(slug);
