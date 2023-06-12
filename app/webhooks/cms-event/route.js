f64cdcbca4e2d29e4cd5d6fd6ffb4854b449e924
Mirko Nasato <mirko@nasato.dev>
2023-06-12 11:00:00 +0100

    On-Demand Revalidation

--- app/webhooks/cms-event/route.js
+++ app/webhooks/cms-event/route.js
@@ -1,5 +1,11 @@
+import { revalidateTag } from 'next/cache';
+import { CACHE_TAG_REVIEWS } from '@/lib/reviews';
+
 export async function POST(request) {
   const payload = await request.json();
-  console.log('payload:', payload);
+  if (payload.model === 'review') {
+    revalidateTag(CACHE_TAG_REVIEWS);
+    console.log('revalidated:', CACHE_TAG_REVIEWS);
+  }
   return new Response(null, { status: 204 });
 }
