c7115f58156612e7d70a80f0bc373f2b20dc5ab8
Mirko Nasato <mirko@nasato.dev>
2023-06-12 11:00:00 +0100

    Background Revalidation

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import Heading from '@/components/Heading';
 import { getReviews } from '@/lib/reviews';
 
-export const dynamic = 'force-dynamic';
+export const revalidate = 30; // seconds
 
 export const metadata: Metadata = {
   title: 'Reviews',
