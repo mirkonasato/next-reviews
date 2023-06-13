de420d48417c20b0ff99a6c45546284eff7c4d92
Mirko Nasato <mirko@nasato.dev>
2023-06-13 11:00:00 +0100

    Pagination with Strapi

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -12,9 +12,11 @@ export const metadata: Metadata = {
   title: 'Reviews',
 };
 
+const PAGE_SIZE = 6;
+
 export default async function ReviewsPage({ searchParams }: ReviewsPageProps) {
   const page = parsePageParam(searchParams.page);
-  const reviews = await getReviews(6);
+  const reviews = await getReviews(PAGE_SIZE, page);
   console.log('[ReviewsPage] rendering:', page);
   return (
     <>
