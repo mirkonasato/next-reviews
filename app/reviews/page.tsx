39b948649c675cf2c9a8b3aeb6b199693c6187de
Mirko Nasato <mirko@nasato.dev>
2023-06-13 11:00:00 +0100

    Query String Parameters

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -4,17 +4,26 @@ import Link from 'next/link';
 import Heading from '@/components/Heading';
 import { getReviews } from '@/lib/reviews';
 
+interface ReviewsPageProps {
+  searchParams: { page?: string };
+}
+
 export const metadata: Metadata = {
   title: 'Reviews',
 };
 
-export default async function ReviewsPage() {
+export default async function ReviewsPage({ searchParams }: ReviewsPageProps) {
+  const page = parsePageParam(searchParams.page);
   const reviews = await getReviews(6);
-  console.log('[ReviewsPage] rendering:',
-    reviews.map((review) => review.slug).join(', '));
+  console.log('[ReviewsPage] rendering:', page);
   return (
     <>
       <Heading>Reviews</Heading>
+      <div className="flex gap-2 pb-3">
+        <Link href={`/reviews?page=${page - 1}`}>&lt;</Link>
+        <span>Page {page}</span>
+        <Link href={`/reviews?page=${page + 1}`}>&gt;</Link>
+      </div>
       <ul className="flex flex-row flex-wrap gap-3">
         {reviews.map((review, index) => (
           <li key={review.slug}
@@ -32,4 +41,14 @@ export default async function ReviewsPage() {
       </ul>
     </>
   );
+}
+
+function parsePageParam(paramValue: string): number {
+  if (paramValue) {
+    const page = parseInt(paramValue);
+    if (isFinite(page) && page > 0) {
+      return page;
+    }
+  }
+  return 1;
 }
