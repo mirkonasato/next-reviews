1067d0b43805aeab83f04081e1be3184e688602f
Mirko Nasato <mirko@nasato.dev>
2023-06-14 11:00:00 +0100

    Client-Only Rendering

--- components/SearchBox.jsx
+++ components/SearchBox.jsx
@@ -1,8 +1,15 @@
 'use client';
 
 import { Combobox } from '@headlessui/react';
+import { useIsClient } from '@/lib/hooks';
 
 export default function SearchBox() {
+  const isClient = useIsClient();
+
+  console.log('[SearchBox] isClient:', isClient);
+  if (!isClient) {
+    return null;
+  }
   return (
     <Combobox>
       <Combobox.Input placeholder="Search…" />
