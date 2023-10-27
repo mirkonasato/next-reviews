091990916230e98d2bf4765496a1e1459dad93d9
Mirko Nasato <mirko@nasato.dev>
2023-10-27 17:44:29 +0100

    Exercise: Featured Review

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import Link from 'next/link';
 import Heading from '@/components/Heading';
+import { getFeaturedReview } from '@/lib/reviews';
 
-export default function HomePage() {
+export default async function HomePage() {
+  const review = await getFeaturedReview();
   console.log('[HomePage] rendering');
   return (
     <>
@@ -11,14 +13,14 @@ export default function HomePage() {
       </p>
       <div className="bg-white border rounded shadow w-80
                       hover:shadow-xl sm:w-full">
-        <Link href="/reviews/stardew-valley"
+        <Link href={`/reviews/${review.slug}`}
           className="flex flex-col sm:flex-row">
-          <img src="/images/stardew-valley.jpg" alt=""
+          <img src={review.image} alt=""
             width="320" height="180"
             className="rounded-t sm:rounded-l sm:rounded-r-none"
           />
           <h2 className="font-orbitron font-semibold py-1 text-center sm:px-2">
-            Stardew Valley
+            {review.title}
           </h2>
         </Link>
       </div>
