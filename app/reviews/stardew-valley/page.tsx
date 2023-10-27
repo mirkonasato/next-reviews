38289d892572e7bd13389e5014444b7ddb8c350b
Mirko Nasato <mirko@nasato.dev>
2023-10-27 17:44:29 +0100

    Page Routes

---
+++ app/reviews/stardew-valley/page.tsx
@@ -0,0 +1,10 @@
+export default function StardewValleyPage() {
+  return (
+    <>
+      <h1>Stardew Valley</h1>
+      <p>
+        This will be the review for Stardew Valley.
+      </p>
+    </>
+  );
+}
