f1c0161c9329cdb3091593e8ddd9e3430e3dd1a7
Mirko Nasato <mirko@nasato.dev>
2023-10-27 17:44:29 +0100

    Page Not Found

---
+++ app/not-found.jsx
@@ -0,0 +1,12 @@
+import Heading from '@/components/Heading';
+
+export default function NotFoundPage() {
+  return (
+    <>
+      <Heading>Not Found</Heading>
+      <p>
+        Oops, the page you requested only exists in a parallel universe.
+      </p>
+    </>
+  );
+}
