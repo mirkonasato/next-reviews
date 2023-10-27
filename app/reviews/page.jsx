a7b66625e23ce4f3df7310bd155a0f53286fa125
Mirko Nasato <mirko@nasato.dev>
2023-10-27 17:44:29 +0100

    Exercise: Review Links

--- app/reviews/page.jsx
+++ app/reviews/page.jsx
@@ -1,10 +1,21 @@
+import Link from 'next/link';
+
 export default function ReviewsPage() {
   return (
     <>
       <h1>Reviews</h1>
-      <p>
-        Here we'll list all the reviews.
-      </p>
+      <ul>
+        <li>
+          <Link href="/reviews/hollow-knight">
+            Hollow Knight
+          </Link>
+        </li>
+        <li>
+          <Link href="/reviews/stardew-valley">
+            Stardew Valley
+          </Link>
+        </li>
+      </ul>
     </>
   );
 }
