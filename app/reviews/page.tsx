f0751ac3c9cfa3790ab2a3bc80b521053f214bce
Mirko Nasato <mirko@nasato.dev>
2023-10-27 17:44:29 +0100

    Dynamic Rendering

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -4,6 +4,8 @@ import Link from 'next/link';
 import Heading from '@/components/Heading';
 import { getReviews } from '@/lib/reviews';
 
+export const dynamic = 'force-dynamic';
+
 export const metadata: Metadata = {
   title: 'Reviews',
 };
