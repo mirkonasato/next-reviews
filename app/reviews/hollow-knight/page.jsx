5dd146e38def8fea63ca93567a4cde018dd99e27
Mirko Nasato <mirko@nasato.dev>
2023-10-27 17:44:29 +0100

    Exercise: Add More Pages

---
+++ app/reviews/hollow-knight/page.jsx
@@ -0,0 +1,10 @@
+export default function HollowKnightPage() {
+  return (
+    <>
+      <h1>Hollow Knight</h1>
+      <p>
+        This will be the review for Hollow Knight.
+      </p>
+    </>
+  );
+}
