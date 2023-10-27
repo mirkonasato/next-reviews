f9ddc7d6454a9411446ab390e2fc483697c797ce
Mirko Nasato <mirko@nasato.dev>
2023-10-27 17:44:29 +0100

    Static Assets

--- app/reviews/hollow-knight/page.tsx
+++ app/reviews/hollow-knight/page.tsx
@@ -4,6 +4,9 @@ export default function HollowKnightPage() {
   return (
     <>
       <Heading>Hollow Knight</Heading>
+      <img src="/images/hollow-knight.jpg" alt=""
+        width="640" height="360" className="mb-2 rounded"
+      />
       <p>
         This will be the review for Hollow Knight.
       </p>
--- app/reviews/stardew-valley/page.tsx
+++ app/reviews/stardew-valley/page.tsx
@@ -4,6 +4,9 @@ export default function StardewValleyPage() {
   return (
     <>
       <Heading>Stardew Valley</Heading>
+      <img src="/images/stardew-valley.jpg" alt=""
+        width="640" height="360" className="mb-2 rounded"
+      />
       <p>
         This will be the review for Stardew Valley.
       </p>
