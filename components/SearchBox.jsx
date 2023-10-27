5fcad89d5fe1df1418958e868a003808aa8f08a8
Mirko Nasato <mirko@nasato.dev>
2023-10-27 17:44:29 +0100

    Debouncing Requests

--- components/SearchBox.jsx
+++ components/SearchBox.jsx
@@ -3,18 +3,20 @@
 import { Combobox } from '@headlessui/react';
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
+import { useDebounce } from 'use-debounce';
 import { useIsClient } from '@/lib/hooks';
 
 export default function SearchBox() {
   const router = useRouter();
   const isClient = useIsClient();
   const [query, setQuery] = useState('');
+  const [debouncedQuery] = useDebounce(query, 300);
   const [reviews, setReviews] = useState([]);
   useEffect(() => {
-    if (query.length > 1) {
+    if (debouncedQuery.length > 1) {
       const controller = new AbortController();
       (async () => {
-        const url = '/api/search?query=' + encodeURIComponent(query);
+        const url = '/api/search?query=' + encodeURIComponent(debouncedQuery);
         const response = await fetch(url, { signal: controller.signal });
         const reviews = await response.json();
         setReviews(reviews);
@@ -23,13 +25,13 @@ export default function SearchBox() {
     } else {
       setReviews([]);
     }
-  }, [query]);
+  }, [debouncedQuery]);
 
   const handleChange = (review) => {
     router.push(`/reviews/${review.slug}`);
   };
 
-  // console.log('[SearchBox] query:', query);
+  // console.log('[SearchBox]', { query, debouncedQuery });
   if (!isClient) {
     return null;
   }
