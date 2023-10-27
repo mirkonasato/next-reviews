ac0a25e3e761763df6b4862f510086cd3e41e776
Mirko Nasato <mirko@nasato.dev>
2023-10-27 17:44:29 +0100

    Title Template

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,5 +1,9 @@
 import Heading from '@/components/Heading';
 
+export const metadata = {
+  title: 'About',
+};
+
 export default function AboutPage() {
   return (
     <>
--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,13 @@ import NavBar from '../components/NavBar';
 import { exo2, orbitron } from './fonts';
 import './globals.css';
 
+export const metadata = {
+  title: {
+    default: 'Indie Gamer',
+    template: '%s | Indie Gamer',
+  },
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${exo2.variable} ${orbitron.variable}`}>
