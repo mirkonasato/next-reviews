d6d49a1a89ae480b7ef4f7ab78ed02fdbf1b90cf
Mirko Nasato <mirko@nasato.dev>
2023-10-27 17:44:29 +0100

    Component State

--- components/ShareLinkButton.jsx
+++ components/ShareLinkButton.jsx
@@ -1,15 +1,22 @@
 'use client';
 
+import { useState } from 'react';
+
 export default function ShareLinkButton() {
+  const [clicked, setClicked] = useState(false);
+
   const handleClick = () => {
-    console.log('clicked!');
+    navigator.clipboard.writeText(window.location.href);
+    setClicked(true);
+    setTimeout(() => setClicked(false), 1500);
   };
 
+  console.log('[ShareLinkButton] clicked:', clicked);
   return (
     <button onClick={handleClick}
       className="border px-2 py-1 rounded text-slate-500 text-sm
                  hover:bg-orange-100 hover:text-slate-700">
-      Share link
+      {clicked ? 'Link copied!' : 'Share link'}
     </button>
   );
 }
