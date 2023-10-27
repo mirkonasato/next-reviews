3fbb006cd181c009054692168aacc75467eebc26
Mirko Nasato <mirko@nasato.dev>
2023-10-27 17:44:29 +0100

    Configurable Image Source

--- next.config.js
+++ next.config.js
@@ -8,6 +8,17 @@ module.exports = {
         port: '1337',
         pathname: '/uploads/**',
       },
+      toRemotePattern(process.env.CMS_IMAGE_PATTERN),
     ],
   },
 };
+
+function toRemotePattern(urlString) {
+  const url = new URL(urlString);
+  return {
+    protocol: url.protocol.replace(':', ''),
+    hostname: url.hostname,
+    port: url.port,
+    pathname: url.pathname,
+  };
+}
