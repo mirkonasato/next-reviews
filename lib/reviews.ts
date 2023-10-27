b15faf05a8ffd8971fb8e5df3c3595160bdb13e6
Mirko Nasato <mirko@nasato.dev>
2023-10-27 17:44:29 +0100

    Get Reviews From CMS - Step 1

--- lib/reviews.ts
+++ lib/reviews.ts
@@ -1,6 +1,7 @@
 import { readdir, readFile } from 'node:fs/promises';
 import matter from 'gray-matter';
 import { marked } from 'marked';
+import qs from 'qs';
 
 export interface Review {
   slug: string;
@@ -22,15 +23,27 @@ export async function getReview(slug: string): Promise<Review> {
   return { slug, title, date, image, body };
 }
 
+/*
+    slug: 'hellblade',
+    title: 'Hellblade',
+    date: '2023-05-06',
+    image: '/images/hellblade.jpg',
+*/
 export async function getReviews(): Promise<Review[]> {
-  const slugs = await getSlugs();
-  const reviews: Review[] = [];
-  for (const slug of slugs) {
-    const review = await getReview(slug);
-    reviews.push(review);
-  }
-  reviews.sort((a, b) => b.date.localeCompare(a.date));
-  return reviews;
+  const url = 'http://localhost:1337/api/reviews?'
+    + qs.stringify({
+      fields: ['slug', 'title', 'subtitle', 'publishedAt'],
+      populate: { image: { fields: ['url'] } },
+      sort: ['publishedAt:desc'],
+      pagination: { pageSize: 6 },
+    }, { encodeValuesOnly: true });
+  console.log('getReviews:', url);
+  const response = await fetch(url);
+  const { data } = await response.json();
+  return data.map(({ attributes }) => ({
+    slug: attributes.slug,
+    title: attributes.title,
+  }));
 }
 
 export async function getSlugs(): Promise<string[]> {
