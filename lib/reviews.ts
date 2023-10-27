f8b955ee7115fab00082bd17db076a2507fb55f1
Mirko Nasato <mirko@nasato.dev>
2023-10-27 17:44:29 +0100

    Data Layer Separation

---
+++ lib/reviews.ts
@@ -0,0 +1,17 @@
+import { readFile } from 'node:fs/promises';
+import matter from 'gray-matter';
+import { marked } from 'marked';
+
+export interface Review {
+  title: string;
+  date: string;
+  image: string;
+  body: string;
+}
+
+export async function getReview(slug: string): Promise<Review> {
+  const text = await readFile(`./content/reviews/${slug}.md`, 'utf8');
+  const { content, data: { title, date, image } } = matter(text);
+  const body = marked(content);
+  return { title, date, image, body };
+}
