9240e5b66fb4ff72971c69f711b99355eaaff11f
Mirko Nasato <mirko@nasato.dev>
2023-10-27 17:44:29 +0100

    Rendering Arrays

--- app/reviews/page.jsx
+++ app/reviews/page.jsx
@@ -4,31 +4,23 @@ import { getReviews } from '@/lib/reviews';
 
 export default async function ReviewsPage() {
   const reviews = await getReviews();
-  console.log('[ReviewsPage] reviews:', reviews);
   return (
     <>
       <Heading>Reviews</Heading>
-      <ul className="flex flex-col gap-3">
-        <li className="bg-white border rounded shadow w-80 hover:shadow-xl">
-          <Link href="/reviews/hollow-knight">
-            <img src="/images/hollow-knight.jpg" alt=""
-              width="320" height="180" className="rounded-t"
-            />
-            <h2 className="font-orbitron font-semibold py-1 text-center">
-              Hollow Knight
-            </h2>
-          </Link>
-        </li>
-        <li className="bg-white border rounded shadow w-80 hover:shadow-xl">
-          <Link href="/reviews/stardew-valley">
-            <img src="/images/stardew-valley.jpg" alt=""
-              width="320" height="180" className="rounded-t"
-            />
-            <h2 className="font-orbitron font-semibold py-1 text-center">
-              Stardew Valley
-            </h2>
-          </Link>
-        </li>
+      <ul className="flex flex-row flex-wrap gap-3">
+        {reviews.map((review) => (
+          <li key={review.slug}
+            className="bg-white border rounded shadow w-80 hover:shadow-xl">
+            <Link href={`/reviews/${review.slug}`}>
+              <img src={review.image} alt=""
+                width="320" height="180" className="rounded-t"
+              />
+              <h2 className="font-orbitron font-semibold py-1 text-center">
+                {review.title}
+              </h2>
+            </Link>
+          </li>
+        ))}
       </ul>
     </>
   );
