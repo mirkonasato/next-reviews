757260c15caca1c1ac4dcb78961c610c57ec6397
Mirko Nasato <mirko@nasato.dev>
2023-10-27 17:44:29 +0100

    Layout Styling

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,10 +4,10 @@ import './globals.css';
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="flex flex-col px-4 py-2 min-h-screen">
         <header>
           <nav>
-            <ul>
+            <ul className="flex gap-2">
               <li>
                 <Link href="/">Home</Link>
               </li>
@@ -20,10 +20,10 @@ export default function RootLayout({ children }) {
             </ul>
           </nav>
         </header>
-        <main>
+        <main className="grow py-3">
           {children}
         </main>
-        <footer>
+        <footer className="border-t py-3 text-center text-xs">
           Game data and images courtesy
           of <a href="https://rawg.io/" target="_blank">RAWG</a>
         </footer>
