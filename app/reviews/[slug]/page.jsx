a26cf9f0ddf5f24de1c54f9307e74995677fa824
Mirko Nasato <mirko@nasato.dev>
2023-10-27 17:44:29 +0100

    Share Link Button

--- app/reviews/[slug]/page.jsx
+++ app/reviews/[slug]/page.jsx
@@ -1,4 +1,5 @@
 import Heading from '@/components/Heading';
+import ShareLinkButton from '@/components/ShareLinkButton';
 import { getReview, getSlugs } from '@/lib/reviews';
 
 export async function generateStaticParams() {
@@ -19,7 +20,10 @@ export default async function ReviewPage({ params: { slug } }) {
   return (
     <>
       <Heading>{review.title}</Heading>
-      <p className="italic pb-2">{review.date}</p>
+      <div className="flex gap-3 items-baseline">
+        <p className="italic pb-2">{review.date}</p>
+        <ShareLinkButton />
+      </div>
       <img src={review.image} alt=""
         width="640" height="360" className="mb-2 rounded"
       />
