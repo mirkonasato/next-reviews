9d50ef32c898a4f3176fa34437cd3e8a1cd0b1eb
Mirko Nasato <mirko@nasato.dev>
2023-10-27 17:44:29 +0100

    Home Page

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,7 +2,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        {children}
+        <header>
+          [header]
+        </header>
+        <main>
+          {children}
+        </main>
+        <footer>
+          [footer]
+        </footer>
       </body>
     </html>
   );
---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+export default function HomePage() {
+  return (
+    <>
+      <h1>Indie Gamer</h1>
+      <p>
+        Only the best indie games, reviewed for you.
+      </p>
+    </>
+  );
+}
