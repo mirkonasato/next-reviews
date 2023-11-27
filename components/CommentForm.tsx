24e160b728b829d1aee9ff6be283da960c09b6a7
Mirko Nasato <mirko@nasato.dev>
2023-11-27 17:44:45 +0000

    Preventing Duplicate Submissions

--- components/CommentForm.tsx
+++ components/CommentForm.tsx
@@ -10,19 +10,25 @@ export interface CommentFormProps {
   title: string;
 }
 
+interface SubmissionState {
+  loading: boolean;
+  error: ActionError | null;
+}
+
 export default function CommentForm({ slug, title }: CommentFormProps) {
-  const [error, setError] = useState<ActionError | null>(null);
+  const [state, setState] = useState<SubmissionState>({ loading: false, error: null });
 
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    setError(null);
+    setState({ loading: true, error: null });
     const form = event.currentTarget;
     const formData = new FormData(form);
     const result = await createCommentAction(formData);
     if (result?.isError) {
-      setError(result);
+      setState({ loading: false, error: result });
     } else {
       form.reset();
+      setState({ loading: false, error: null });
     }
   };
 
@@ -49,12 +55,13 @@ export default function CommentForm({ slug, title }: CommentFormProps) {
           className="border px-2 py-1 rounded w-full"
         />
       </div>
-      {Boolean(error) && (
-        <p className="text-red-700">{error.message}</p>
+      {Boolean(state.error) && (
+        <p className="text-red-700">{state.error.message}</p>
       )}
-      <button type="submit"
+      <button type="submit" disabled={state.loading}
         className="bg-orange-800 rounded px-2 py-1 self-center
-                   text-slate-50 w-32 hover:bg-orange-700">
+                   text-slate-50 w-32 hover:bg-orange-700
+                   disabled:bg-slate-500 disabled:cursor-not-allowed">
         Submit
       </button>
     </form>
