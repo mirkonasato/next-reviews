76e177671fae1bd9052bcf9530faf011faa9742a
Mirko Nasato <mirko@nasato.dev>
2023-11-27 17:44:45 +0000

    Prisma Client and Dev Server

--- lib/comments.ts
+++ lib/comments.ts
@@ -1,4 +1,13 @@
+import type { Comment } from '@prisma/client';
 import { db } from './db';
+
+export type CreateCommentData = Omit<Comment, 'id' | 'postedAt'>;
+
+export async function createComment({ slug, user, message }: CreateCommentData) {
+  return await db.comment.create({
+    data: { slug, user, message },
+  });
+}
 
 export async function getComments(slug: string) {
   return await db.comment.findMany({
--- lib/db.ts
+++ lib/db.ts
@@ -1,5 +1,12 @@
 import { PrismaClient } from '@prisma/client';
 
-export const db = new PrismaClient({
-  // log: [{ emit: 'stdout', level: 'query' }],
-});
+export const db = createPrismaClient();
+
+function createPrismaClient(): PrismaClient {
+  if (!globalThis.prismaClient) {
+    globalThis.prismaClient = new PrismaClient({
+      // log: [{ emit: 'stdout', level: 'query' }],
+    });
+  }
+  return globalThis.prismaClient;
+}
