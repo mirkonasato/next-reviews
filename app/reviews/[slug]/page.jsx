d11164181247be8f6aae3c151d51db9879c2bd65
Mirko Nasato <mirko@nasato.dev>
2023-11-27 17:44:45 +0000

    Inline Server Action

--- app/reviews/[slug]/page.jsx
+++ app/reviews/[slug]/page.jsx
@@ -50,7 +50,7 @@ export default async function ReviewPage({ params: { slug } }) {
           <ChatBubbleBottomCenterTextIcon className="h-6 w-6" />
           Comments
         </h2>
-        <CommentForm title={review.title} />
+        <CommentForm slug={slug} title={review.title} />
         <CommentList slug={slug} />
       </section>
     </>
--- components/CommentForm.jsx
+++ components/CommentForm.jsx
@@ -1,6 +1,19 @@
-export default function CommentForm({ title }) {
+import { createComment } from '@/lib/comments';
+
+export default function CommentForm({ slug, title }) {
+  async function action(formData) {
+    'use server';
+    const comment = await createComment({
+      slug,
+      user: formData.get('user'),
+      message: formData.get('message'),
+    });
+    console.log('created:', comment);
+  }
+
   return (
-    <form className="border bg-white flex flex-col gap-2 mt-3 px-3 py-3 rounded">
+    <form action={action}
+      className="border bg-white flex flex-col gap-2 mt-3 px-3 py-3 rounded">
       <p className="pb-1">
         Already played <strong>{title}</strong>? Have your say!
       </p>
@@ -8,13 +21,17 @@ export default function CommentForm({ title }) {
         <label htmlFor="userField" className="shrink-0 w-32">
           Your name
         </label>
-        <input id="userField" className="border px-2 py-1 rounded w-48" />
+        <input id="userField" name="user"
+          className="border px-2 py-1 rounded w-48"
+        />
       </div>
       <div className="flex">
         <label htmlFor="messageField" className="shrink-0 w-32">
           Your comment
         </label>
-        <textarea id="messageField" className="border px-2 py-1 rounded w-full"  />
+        <textarea id="messageField" name="message"
+          className="border px-2 py-1 rounded w-full"
+        />
       </div>
       <button type="submit"
         className="bg-orange-800 rounded px-2 py-1 self-center
